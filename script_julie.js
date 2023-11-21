f55679249a250c3cc14ae99bf0fd1fd9f8956994
julieATS <146748819+julieATS@users.noreply.github.com>
2023-11-21 16:24:19 +0100

    màj 21/11
    input utilisateur OK
    affichage données OK

--- script_julie.js
+++ script_julie.js
@@ -1,39 +1,32 @@
-
-/*
-fetch ("https://api.le-systeme-solaire.net/rest/bodies/").then(function(response) {
-    console.log("success!",response)
-    //document.getElementById("test").innerHTML= "sucess!" + response
-})
-*/
-/*
-fetch ("https://api.le-systeme-solaire.net/rest/bodies/{sun}").then(function(response) {
-    if (response.ok) {
-        return response.json()
-    }
-    //console.log("success!",response)
-   
-})
-.then(data => {
-    console.log(data)
-    document.getElementById("test").innerHTML= data.name + " " + data.bodyType
-
-})
-*/
-
 const research = () => {
-    x = document.getElementById("input").value
-    console.log(x)
-    fetch(`https://api.le-systeme-solaire.net/rest/bodies/{${x}}`).then(function (response) {
-      if (response.ok) {
-        return response.json()
-      }
-      console.log("success!",response)
+
+  fetch(`https://api.le-systeme-solaire.net/rest/bodies/{${document.getElementById("Name").value}}`).then(function (response) {
+    if (response.ok) {
+      return response.json()
+    }
+    console.log("success!",response)
+  })
+
+    .then(data => {
+      console.log(data)
+      document.getElementById("test").innerHTML = "nom : " + data.name + "<br>"
+      document.getElementById("test").innerHTML += "type : " + data.bodyType + "<br>"
+      document.getElementById("test").innerHTML += "température : " + data.avgTemp + " K°" + "<br>"
+      document.getElementById("test").innerHTML += "masse : " + data.mass.massValue.toFixed(2)*10**data.mass.massExponent + "Kg" + "<br>"
+      document.getElementById("test").innerHTML += "lune.s : " + data.moons.length + "<br>"
+
     })
-  
-      .then(data => {
-        console.log(data)
-        document.getElementById("test").innerHTML = data.name + " " + data.bodyType
-      })
+}
+
+fetch(`https://api.le-systeme-solaire.net/rest/bodies/{mars}`).then(function (response) {
+  if (response.ok) {
+    return response.json()
   }
-  
-  //research()
+  console.log("success!",response)
+})
+
+  .then(data => {
+    console.log(data)
+
+  })
+
