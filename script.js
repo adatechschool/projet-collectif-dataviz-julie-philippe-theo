f51710775a3eef0b59f5ac0a40b3dad0a5eb9cbb
julieATS <146748819+julieATS@users.noreply.github.com>
2023-11-23 10:12:08 +0100

    fonction sortBy() à compléter

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const research = () => {
 
-  fetch(`https://api.le-systeme-solaire.net/rest/bodies/{${document.getElementById("Name").value}}`).then(function (response) {
+  fetch(`https://api.le-systeme-solaire.net/rest/bodies/{${document.getElementById("name").value}}`).then(function (response) {
     if (response.ok) {
       return response.json()
     }
@@ -18,20 +18,25 @@ const research = () => {
     })
 }
 
-//filter[]=isPlanet,eq,true&data=name,avgTemp&order=avgTemp,asc
-fetch("https://api.le-systeme-solaire.net/rest/bodies?filter[]=isPlanet,eq,true&data=name,avgTemp&order=avgTemp,asc")
-.then(function (response) {
-  if (response.ok) {
-    return response.json()
+let sortBy = () => {
+  input = document.getElementById("sort").value
+  if (input == "température") {
+    fetch("https://api.le-systeme-solaire.net/rest/bodies?filter[]=isPlanet,eq,true&data=name,avgTemp&order=avgTemp,asc")
+    .then(function (response) {
+      if (response.ok) {
+      return response.json()
+      }
+      console.log("success!",response)
+    })
+    .then(data => {
+      console.log(data)
+      document.getElementById("filter").innerHTML= "" //remet à jour pour le nouveau filtre
+      for (let i = 0; i <8; i++) {
+        document.getElementById("filter").innerHTML+= data.bodies[i].name + " : " + data.bodies[i].avgTemp + " K°" + "<br>"
+      }
+    })
   }
-  console.log("success!",response)
-})
-
-  .then(data => {
-    console.log(data)
-    document.getElementById("filter").innerHTML= "" //remet à jour pour le nouveau filtre
-    for (let i = 0; i <8; i++) {
-      document.getElementById("filter").innerHTML+= data.bodies[i].name + " : " + data.bodies[i].avgTemp + " K°" + "<br>"
-    }
-  })
-
+  else {
+    document.getElementById("filter").innerHTML= "mot invalide"
+  }
+}
